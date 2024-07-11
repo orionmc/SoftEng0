54cfd7864522072671e8b88c6a827f0d3fcf2693
Nihat Yumer <>
2024-07-11 14:38:00 +0100

    commit 1e3f3b3b1

---
+++ SoftEng0.py
@@ -0,0 +1,16 @@
+import csv
+import pandas as pd
+import seaborn as sns
+
+
+heartds = pd.read_csv('https://raw.githubusercontent.com/orionmc/SoftEng0/main/heart0.csv')
+heartds.tail(6)
+
+#heartds.to_csv(r'c:\mytestprj\heart0.csv', index=False)
+
+
+
+
+
+
+
