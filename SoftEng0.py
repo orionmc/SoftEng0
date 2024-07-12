e36e22b6b4893f41dbb3ec5ddc7b93ed2e785083
Nihat Yumer <>
2024-07-12 16:15:35 +0100

    commit

--- SoftEng0.py
+++ SoftEng0.py
@@ -1,14 +1,15 @@
 import csv
 import pandas as pd
 import seaborn as sns
-
+import os
+import matplotlib.pyplot as plt
 
 heartds = pd.read_csv('https://raw.githubusercontent.com/orionmc/SoftEng0/main/heart0.csv')
 
 #For quick overview of the data we can use either tail or head functions
-#print(heartds.tail(6))
-#print(heartds.head(6))
-#heartds.to_csv(r'c:\mytestprj\heart0.csv', index=False) # write the dataframe to csv file on specified location.
+print("\nLast 6 rows\n",heartds.tail(6),sep=os.linesep)
+print("\nFirst 6 rows\n",heartds.head(6),sep=os.linesep)
+heartds.to_csv(r'c:\mytestprj\heart0.csv', index=False) # write the dataframe to csv file on specified location.
 
 # check for missing values in the dataset
 #print(heartds[heartds.isna().any(axis=1)]) # print the rows with missing values
@@ -24,7 +25,9 @@ heartds_no_missing = heartds.dropna()   # drop the rows with missing values
 heartds[heartds.duplicated()]           # check for duplicate rows in the dataset
 heartds.drop_duplicates(inplace=True)   # drop the duplicate rows in the dataset
 
+# Print basic stat
+print("\nBasic Stat\n",heartds.describe(),sep=os.linesep)
 
-
-
+sns.countplot(x='sex', data=heartds)
+plt.show()
 
